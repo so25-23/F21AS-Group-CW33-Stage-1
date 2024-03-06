d39b3b33e1ef13615a30c79f611ff792ea0cef68
so25-23 <so25@hw.ac.uk>
2024-03-06 20:44:16 +0000

    Updated with foundation code to build upon

---
+++ f21AS_Stage_1_Code/src/checkinsystem/test/FlightConstructorTest.java
@@ -0,0 +1,14 @@
+package checkinsystem.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import checkinsystem.model.CheckInException;
+import checkinsystem.model.Flight;
+
+public class FlightConstructorTest {
+	
+	// TODO 
+
+}
